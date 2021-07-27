591671492b74c2cd6527864d82fb0f41d9c6ecac
locDEV169 <loc16910@gmail.com>
2021-07-27 15:38:58 +0700

    add dependency aws-java-sdk-s3 with link + Create AmazonConfig.java + làm phần Amazon S3 Client

--- src/main/java/com/amoigoscode/awsimageupload/AwsImageUploadApplication.java
+++ src/main/java/com/amoigoscode/awsimageupload/AwsImageUploadApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AwsImageUploadApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AwsImageUploadApplication.class, args);
 	}
 
