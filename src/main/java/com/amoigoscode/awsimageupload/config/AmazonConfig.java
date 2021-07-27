3d7518b53aa5c3c01afbd52ca2fb9b13c45adb7e
locDEV169 <loc16910@gmail.com>
2021-07-27 15:45:08 +0700

    Xong phần create Amazon S3 Client

--- src/main/java/com/amoigoscode/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/amoigoscode/awsimageupload/config/AmazonConfig.java
@@ -1,17 +1,25 @@
 package com.amoigoscode.awsimageupload.config;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
+    @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "",""
+                "AKIA3QAUKXGMHJP26E5Y",
+                "sE6y4ZFCBAnBTIQj4nE1uPDR1wtwAlJxit4XcVYk"
         );
-        return null;
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
     }
 }
