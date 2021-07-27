f15e25a210e5238eed06e60a4d3c58584382dc28
locDEV169 <loc16910@gmail.com>
2021-07-27 23:48:21 +0700

    làm tới phần react component and Axios

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect} from "react";
+import axios from "axios";
+
+const UserProfiles = () => {
+  const [userProfiles, setUserProfiles] = useState([]);
+
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then((res) => {
+      console.log(res);
+    })
+        .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+
+  return <h2>hello</h2>
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserProfiles />
     </div>
   );
 }
