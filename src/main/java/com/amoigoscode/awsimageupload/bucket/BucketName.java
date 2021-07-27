867b04794a5e5fefbe5b49866e0ead2c8c5a0c69
locDEV169 <loc16910@gmail.com>
2021-07-27 15:56:44 +0700

    create S3 server in Amazon and create S3 BucketName

---
+++ src/main/java/com/amoigoscode/awsimageupload/bucket/BucketName.java
@@ -0,0 +1,16 @@
+package com.amoigoscode.awsimageupload.bucket;
+
+public enum BucketName {
+
+    PROFILE_IMAGE("amoigoscode-image-upload-123");
+
+    private final String bucketName;
+
+    BucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+}
